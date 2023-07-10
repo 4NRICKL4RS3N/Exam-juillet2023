83be75c6c2f70ac1e4a20641ab78102ebeb402ee
nandrainaaa <arakotorazafy@gmail.com>
2023-07-10 13:33:25 +0200

    page d'accueil
    ito

--- application/views/page.php
+++ application/views/page.php
@@ -5,14 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Cotisse</title>
+  <title>Document</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -20,109 +18,231 @@
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <link href="assets/css/page.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-<!-- ======= Header ======= -->
-<header id="header" class="fixed-top d-flex align-items-center">
-<div class="container">
-    <div class="header-container d-flex align-items-center justify-content-between">
-    <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>J'aime</span></a></h1>
-    </div>
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
 
-    <nav id="navbar" class="navbar">
+      <h1 class="logo me-auto"><a href="index.html">J'aime</a></h1>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-        <li><a class="nav-link scrollto active" href="#acceuil">Acceuil</a></li>
-        <li><a class="nav-link scrollto" href="#voyage">Voyage</a></li>
-        <li><a class="nav-link scrollto" href="#information">Informations</a></li>
-        <li><a class="nav-link scrollto " href="#aide">Aide</a></li>
-        <li class="dropdown"><a href="#"><span>Langue</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Liste code</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-            <li class="dropdown"><a href="#"><span>Francais</span></a></li>
-            <li class="dropdown"><a href="#"><span>Anglais</span></a></li>
-            <li class="dropdown"><a href="#"><span>Malagasy</span></a></li>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
             </ul>
-        </li>
-        <li><a class="getstarted scrollto" href="#connecter">Se connecter</a></li>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
-    </nav>
-    </div>
-</div> 
-</header>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
 
-<!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex align-items-center">
-    <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
-    <h1>Bienvenu sur i-Zotra</h1>
-    </div>
-</section><!-- End Hero -->
+      <a href="#appointment" class="appointment-btn scrollto">Se connecter</a>
 
-<!-- ======= Contact Section ======= -->
-<section id="contact" class="contact">
+    </div>
+  </header>
+
+  <section id="hero" class="d-flex align-items-center">
     <div class="container">
-    <div class="row">
-        <div class="col-lg-4" data-aos="fade-right">
+      <h1>Bienvenu sur Regime</h1>
+      <h2>Perdez ou gagnez du poids!</h2>
+      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+    </div>
+  </section>
+
+  <main id="main">
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="content">
+              <h3>Pourquoi choisir Regime?</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+              </p>
+              <div class="text-center">
+                <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 d-flex align-items-stretch">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <h1>Mon objectif</h1>
+                    <select name="objectif" id="objectif">
+                      <option value="Augmenter">Augmenter mon poids</option>
+                      <option value="Reduire">Réduire mon poids</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section id="appointment" class="appointment section-bg">
+      <div class="container">
+
         <div class="section-title">
-            <h2>Itinéraire</h2>
-        </div>
+          <h2>Completez le formulaire</h2>
         </div>
 
-        <form action="#" method="post" role="form" class="php-email-form mt-4">
-            <div class="row">
-            <div class="form-group">
-                <label for="depart">Départ :</label>
-                    <div class="depart">
-                        <select name="depart" class="depart" required>
-                            <option value="1">#</option>
-                        </select>
-                    </div>
+        <form action="#" method="post" role="form" class="php-email-form">
+          <div class="row">
+            <div class="col-md-4 form-group">
+              <input type="text" name="age" class="form-control" id="age" placeholder="Votre age" data-rule="minlen:4" data-msg="S'il vous plait entrez une age valide">
+              <div class="validate"></div>
             </div>
-        
-            <div class="form-group">
-                <label for="arrivee">Arrivée :</label>
-                    <div class="arrivee">
-                        <select name="arrivee" class="arrivee" required>
-                            <option value="1">#</option>
-                        </select>
-                    </div>
+            <div class="col-md-4 form-group mt-3 mt-md-0">
+              <input type="taille" class="form-control" name="taille" id="taille" placeholder="Votre poids actuel" data-rule="taille" data-msg="S'il vous plait entrez un poids valide">
+              <div class="validate"></div>
             </div>
-        
-            <div class="form-group">
-                <label for="date">Date :</label>
-                <input type="date" id="date" name="date" required>
-                </div>
+            <div class="col-md-4 form-group mt-3 mt-md-0">
+              <input type="poids" class="form-control" name="poids" id="poids" placeholder="Le poids que voulez gagner" data-rule="poids">
+              <div class="validate"></div>
             </div>
-
-            <div class="form-group">
-                <label for="voyageurs">Voyageurs :</label>
-                <input type="number" id="voyageurs" name="voyageurs" required>
-                </div>
+            <div class="col-md-4 form-group mt-3 mt-md-0">
+              <input type="poids" class="form-control" name="poids" id="poids" placeholder="Le poids que vous voulez perdre" data-rule="poids">
+              <div class="validate"></div>
             </div>
-            
-            <div class="text-center"><button type="submit">Réserver</button></div>
+          </div>
+          <div class="row">
+            <div class="col-md-4 form-group mt-3">
+              <label for="date">Debut du régime :</label>
+              <input type="date" name="date" id="date" placeholder="Date debut" data-rule="minlen:4" data-msg="S'il vous plait entrez une date valide">
+              <div class="validate"></div>
+            </div>
+            <div class="col-md-4 form-group mt-3">
+              <label for="date">Fin du régime :</label>
+              <input type="date" name="date" id="date" placeholder="Date fin" data-rule="minlen:4" data-msg="S'il vous plait entrez une date valide">
+              <div class="validate"></div>
+            </div>
+          </div>
+          <div class="mb-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your appointment request has been sent successfully. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Envoyer</button></div>
         </form>
+
+      </div>
+    </section>
+
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
+      </div>
+
+      <div class="container">
+        <div class="row mt-5">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>A108 Adam Street, New York, NY 535022</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+1 5589 55488 55s</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+  </main>
+
+  <footer id="footer">
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Regime</span></strong>. All Rights Reserved
+        </div>
+      </div>
     </div>
-    </div>
-</section>
+  </footer>
 
-</main>
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
 
-<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-
-<script src="assets/js/main.js"></script>
+  <script src="assets/js/main.js"></script>
 
 </body>
 
