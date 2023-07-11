ed2c9140a5f5a3882bb623d1ed7011977a7404df
nandrainaaa <arakotorazafy@gmail.com>
2023-07-11 12:00:15 +0200

    affichage backoffice

---
+++ application/views/back.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Backoffice - Liste des clients</title>
+    <link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/back.css">
+</head>
+<body>
+        <nav class="navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">Backoffice</a>
+            <div class="navbar-links">
+            <a class="nav-link" href="#">Accueil</a>
+            <a class="nav-link" href="#">Tableau de bord</a>
+            <a class="nav-link" href="#">Statistiques</a>
+            <a class="nav-link" href="#">Regimes</a>
+            <a class="nav-link" href="#">Sports</a>
+            </div>
+        </div>
+        </nav>
+
+        <div class="container">
+            <h3>Portefeuille</h3>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Solde</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Client 1</td>
+                <td>1000</td>
+                <td><button class="btn btn-success">Accepter</button></td>
+            </tr>
+            <tr>
+                <td>Client 2</td>
+                <td>1000</td>
+                <td><button class="btn btn-success">Accepter</button></td>
+            </tr>
+            <tr>
+                <td>Client 3</td>
+                <td>1000</td>
+                <td><button class="btn btn-success">Accepter</button></td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+
+        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        <script src="assets/js/main.js"></script></body>
+</html>
